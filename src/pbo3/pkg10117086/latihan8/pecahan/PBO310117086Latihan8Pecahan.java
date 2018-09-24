01ad98c60040b5148db91e323378acc148914dc8
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:23:28 -0700

    Pecahaan

---
+++ src/pbo3/pkg10117086/latihan8/pecahan/PBO310117086Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan8.pecahan;
+
+/**
+ *
+ * @author Lenovo
+ * Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float\t: " + f);
+        System.out.println("Double\t:" + d);
+            
+    }
+    
+}
